ac0d15546fcd061696b43cd15bf25aaf2aac648f
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-17 01:54:10 +0400

    if else and import library

--- ifelse.py
+++ ifelse.py
@@ -1,11 +1,13 @@
-x = 0
-
-if x == 0:
-    x +=1
-    print("х был равен 0")
-elif type(x) == type(1) or type(x) == type(1.1):
-    print("х флоат")
+import os
+sayt = input()
+if 'https://' in sayt:
+    os.system('start ' + sayt)
+    print('if')
+elif 'www.' in sayt:
+    sayt = 'https://' + sayt
+    os.system('start ' + sayt)
+    print("elif")
 else:
-    x = 2
-
-print(100/x)
+    sayt = 'https://www.' + sayt
+    os.system('start ' + sayt)
+    print('else')
