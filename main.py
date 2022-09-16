de3df076274283895bc0d53fa681bfa997b25323
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-16 23:32:02 +0400

    Peremennye

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello MIR")
+num = 3
+num = 4
+num2 = 5
+res = num + num2
+print(res)
+
+number1 = number2 = number3 = 22
+print(number1 + number2 + number3)
+print(number1, number2)
+
+n1, n2 = 12, 13
+print(n1, n2)
+
+swap1, swap2 = 335, 447
+swap1, swap2 = swap2, swap1 + num
+print(swap1, swap2)
+
+swap1 -= swap2
+print(swap1)
