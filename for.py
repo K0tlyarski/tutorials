16df0d061e114dde1347ecbd8fc6e13ab6e4b26f
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-19 00:12:03 +0400

    for with range

--- for.py
+++ for.py
@@ -1,11 +1,16 @@
-m = 'stroka text'
-count = 0
-for i in m:
-    if i == 't':
-        continue
-        count += 1
+x = 'абвгдеежзийклмнопрстуфхцчшщ'
+y = input('Vvdeite stroku \n')
 
-    print(i)
-else:
-    print('цикл завершен', count)
-print('progrm work')
+for i in x:
+    count = 0
+    for r in y:
+        if i == r:
+            count+=1
+    if count>0:
+
+        print('Букв ',i ,' ', count)
+
+k = 0
+#range(start,stop,step)
+for k in range(2,10,3):
+    print(k)
