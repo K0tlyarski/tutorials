215130a9ac2b6a33f67d41432298bc98c79f9d19
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-21 21:32:32 +0400

    функции с переменным количеством аргументов

--- def2.py
+++ def2.py
@@ -1,9 +1,11 @@
-def ExclusiveItem(*args):
+def ExclusiveItem(*args, key = False): #позиционные параметры пере аргс, ключевые параметры после аргс
     NewList = []
     for i in args:
         for y in i:
             if y not in NewList:
                 NewList.append(y)
+    if key == True:
+        NewList.sort()
 
     return NewList
 
@@ -11,5 +13,5 @@ z = [9, 8 ,7, 6]
 x = [8, 8, 9, 7 ,6 ,5]
 c = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]
 
-t = ExclusiveItem(z,x,c)
+t = ExclusiveItem(z,x,c, key = True)
 print(t)
