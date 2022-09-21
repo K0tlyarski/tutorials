8c00d4c88071750e3778db47dd5dd53e781c4e4f
K0tlyarski <djgame2.9.6@gmail.com>
2022-09-21 21:55:50 +0400

    функция в функции

--- def3.py
+++ def3.py
@@ -1,11 +1,10 @@
 x = 5
 def name():
-    x = 24
-
-    return name2(x)
-
-
-
-def name2(par):
-    print(par)
+    x = 333
+    def name2():
+        x = 100
+        print(x)
+    name2()
+    print(x)
 name()
+print(x)
